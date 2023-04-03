1ec747a7b4cb307aae5b59f65136ed211dcb851e
devulapallisai <ee20btech11013@iith.ac.in>
2023-04-03 11:25:07 +0530

    added new changes

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import cv2
 import os
 import numpy as np
+import time
+import matplotlib.pyplot as plt
 
 
-def gabor_filters(ksize=5, sigma=5, theta=np.pi/10, lambd=10, gamma=1):
+def gabor_filters(ksize=5, sigma=5, theta=10, lambd=1, gamma=1, scale_start=0.5, scale_end=1.5, step=0.2):
     ''' This function will precompute scaled and rotated versions of Gabor filters
 
     INPUT PARAMETERS :
@@ -13,11 +15,17 @@ def gabor_filters(ksize=5, sigma=5, theta=np.pi/10, lambd=10, gamma=1):
 
     sigma      = standard deviation for the Gabor filter
 
-    theta      = angle of rotation 
+    theta      = number of angles that are into consideration
 
     lambd      = wavelength of filter (can be attributed to scale change)
 
     gamma      = gamma correction factor
+
+    scale_start= varying scale of Gabor filter starting  value
+
+    scale_end  = varying scale of Gabor filter end value
+
+    step       = step size of the varying scale of the above Gabor filters
 
     RETURNS :
 
@@ -27,19 +35,20 @@ def gabor_filters(ksize=5, sigma=5, theta=np.pi/10, lambd=10, gamma=1):
     # Define the Gabor filter bank
     filters = []
 
-    # number of angles for different rotations
-    no_of_angles = int(np.round(2*(np.pi)/theta))
+    # each angle given there are theta number of angles into consideration also can think as angular resolution
+    angle = (2*np.pi)/theta
 
-    for scale in np.arange(0.5, 1.5, 0.1):
-        # scales changing from 0.5 to 1.5 with each step of size 0.1
+    for scale in np.arange(scale_start, scale_end, step):
+        # scales changing from start to end with each step of size step
         scale_filters = []
-        for j in range(no_of_angles):
-            lambd = lambd
-            gamma = gamma
+        for j in range(theta):
             gabor_filter = cv2.getGaborKernel(
-                (ksize, ksize), sigma, j*theta, lambd*scale, gamma, 0, ktype=cv2.CV_32F)
-            scale_filters.append(gabor_filter)
-        filters.append(scale_filters)
+                (ksize, ksize), np.sqrt(scale)*sigma, j*angle, scale*lambd, gamma, 0, ktype=cv2.CV_32F)
+            scale_filters.append(gabor_filter/np.sum(gabor_filter))
+
+        # rotation invariant scaled version of Gabor filter
+        averaged_value = np.sum(scale_filters, axis=0)
+        filters.append(averaged_value)
 
     return filters
 
@@ -47,94 +56,125 @@ def gabor_filters(ksize=5, sigma=5, theta=np.pi/10, lambd=10, gamma=1):
 def generate_blocks(I, block_size=8):
     ''' This function will return sub blocks of image I
 
-    INPUT PARAMETERS :
+    INPUT PARAMETERS:
 
-    I          = grayscale image 
+    I = grayscale image
 
     block_size = Image is divided into square blocks with each size of this variable for further processing
-                 (type = integer)
+                 (type=integer)
 
-    RETURNS :
+    RETURNS:
 
-    Square blocks of size (block_size,block_size) from image I
+    square overlapping blocks of size(block_size, block_size) from image I (2D array)
 
     '''
     blocks = []
-    for i in range(0, len(I), block_size):
-        for j in range(0, len(I[0]), block_size):
+    for i in range(0, len(I)-block_size+1):
+        t = []
+        for j in range(0, len(I[0])-block_size+1):
             block = I[i:i+block_size, j:j+block_size]
-            blocks.append(block)
+            t.append(block)  # (M-B+1)*(M-B+1) number of blocks
+        blocks.append(t)
     return blocks
 
 
 def feature_matrix_generation(blocks, filters):
-    ''' This function will return feature matrix of whole image 
+    ''' This function will return feature matrix of whole image
 
-    INPUT PARAMETERS :
+    INPUT PARAMETERS:
 
-    blocks  = generated sub-blocks of image I 
+    blocks = generated sub-blocks of image I
 
     filters = set of Gabor filters with rotated and scaled versions
 
-    RETURNS :
+    RETURNS:
 
-    Square a row sorted matrix each representing feature vector of sub-block which has mean of all possible rotations for a given scale
+    Square a row sorted matrix each representing feature vector of sub-block which has mean of all possible
+    rotations for a given scale
 
     '''
-    block_features = []
-    for block in blocks:
-        feature_vector = []
-        for gabor_filters_rot in filters:
-            filtered = []
-            for filter in gabor_filters_rot:
-                filtered_single = cv2.filter2D(block, cv2.CV_8UC3, filter)
-                filtered.append(filtered_single)
-            feature_vector.append(np.mean(filtered))
+    block_features = np.zeros(
+        (len(blocks), len(blocks[0])), dtype=[('f_vector', np.ndarray, len(filters)),
+                                              ('x', int), ('y', int)])
+    for i in range(len(blocks)):
+        for j in range(len(blocks[i])):
+            filtered = np.array(
+                [cv2.filter2D(blocks[i][j], cv2.CV_8UC3, filter) for filter in filters])
+            feature_vector = np.mean(filtered, axis=(1, 2))
             # feature_vector has rotation invariant for given scale
-        # sorting feature vector for each block
-        feature_vector = np.sort(feature_vector)
-        block_features.append(feature_vector)
-
+            # storing feature vector of each block along with its top left corner coordinates
+            block_features[i][j]["f_vector"] = feature_vector
+            block_features[i][j]["x"] = i
+            block_features[i][j]["y"] = j
+            # at each i,j position of Image I compute feature vector
+    block_features = np.argsort(block_features, order='f_vector')
+    # lexographically sorting the above feature matrix
     return block_features
 
 
-def euclidean_distance(A, B):
-    '''Returns euclidean similarity measure for vectors A and B
+def detection(block_features_matrix, Nf=3, Nd=16, D=3):
+    ''' This function will return whether image is an example of copy move forgery or not
 
-    INPUT PARAMETERS :
+    INPUT PARAMETERS:
 
-    A and B = vector of integers 
+    block_features_matrix = feature matrix A
 
-    RETURNS :
+    D = Euclidean Similarity Threshold
 
-    euclidean distance also called l2-norm of A and B
+    Nf = Neighbourhood threshold
+
+    Nd = Eucleadian distance Threshold
+
+    RETURNS:
+
+    True or False
 
     '''
-
-    return np.linalg.norm(A-B)
+    # Compare each block with all other blocks
+    for i in range(len(block_features_matrix)):
+        # this will have all boolean matches between vectors i and j for all j<Nf+i
+        truth_vector = []
+        for j in range(0, np.min(i+Nf, len(block_features_matrix))):
+            # j-i < Nf
+            if (np.linalg.norm(block_features_matrix[i]["feature_vector"]-block_features_matrix[j]["feature_vector"]) < D):
+                d = np.array([block_features_matrix[i]["x"]-block_features_matrix[j]["x"],
+                              block_features_matrix[i]["y"]-block_features_matrix[j]["y"]])
+                if (np.linalg.norm(d) > Nd):
+                    truth_vector.append(True)
+                    # similarity found i.e possible copy move
+                else:
+                    truth_vector.append(False)
+                    # No similarity found i.e no copy move
+            else:
+                truth_vector.append(False)
+        truth_vector = np.array(truth_vector)
+        if (truth_vector.any()):
+            # for all blocks whether it is true or not
+            return True
+    return False
 
 
 def detect_copy_move(image_path, filters, block_size=8, D=3, Nf=3, Nd=16):
     ''' This function will return whether image is an example of copy move forgery or not
 
-    INPUT PARAMETERS :
+    INPUT PARAMETERS:
 
-    image_path = path for the image in the local directory that you are working so that 
-                 opencv can load the image  (type : string)
+    image_path = path for the image in the local directory that you are working so that
+                 opencv can load the image(type: string)
 
     block_size = Image is divided into square blocks with each size of this variable for further processing
-                 (type = integer)
+                 (type=integer)
 
-    filters    = Generated Gabor filters with different scales and rotation similar to SIFT
-                 (type = array of 2D array where 2D array is of type complex)
+    filters = Generated Gabor filters with different scales and rotation similar to SIFT
+                 (type=array of 2D array where 2D array is of type complex)
 
-    D          = Euclidean Similarity Threshold 
+    D = Euclidean Similarity Threshold
 
-    Nf         = Neighbourhood threshold 
+    Nf = Neighbourhood threshold
 
-    Nd         = Eucleadian distance Threshold
+    Nd = Eucleadian distance Threshold
 
-    RETURNS :
+    RETURNS:
 
     True or False depending whether image is original or forged
 
@@ -151,26 +191,7 @@ def detect_copy_move(image_path, filters, block_size=8, D=3, Nf=3, Nd=16):
     # Apply the Gabor filters to each block and getting feature_matrix
     block_features_matrix = feature_matrix_generation(blocks, filters)
 
-    # Compare each block with all other blocks
-    for i in range(len(block_features_matrix)):
-        # this will have all boolean matches between vectors i and j for all j<Nf+i
-        truth_vector = []
-        for j in range(0, i+Nf):
-            '''j-i<Nf'''
-            if (euclidean_distance(block_features_matrix[i], block_features_matrix[j]) < D):
-                if (block_size*euclidean_distance(np.array([i, i]), np.array([j, j])) > Nd):
-                    truth_vector.append(False)
-                    # similarity found i.e possible copy move
-                else:
-                    truth_vector.append(True)
-                    # No similarity found i.e no copy move
-            else:
-                truth_vector.append(True)
-        truth_vector = np.array(truth_vector)
-        if (truth_vector.any()):
-            # for all blocks whether it is true or not
-            return True
-    return False
+    return detection(block_features_matrix, Nf, Nd, D)
 
 
 paths = []
@@ -183,13 +204,13 @@ file_formats = [".png", ".jpg", ".jpeg"]  # image file formats
 # --------------------------GABOR FILTERS GENERATION ---------------------------------
 filter_size = 5
 filter_standard_dev = 10
-filter_theta = np.pi/30  # theta
-filters = gabor_filters(filter_size, filter_standard_dev, filter_theta)
+no_filter_theta = 12  # theta angluar resolution = 30degree
+filters = gabor_filters(filter_size, filter_standard_dev, no_filter_theta)
 # ------------------------------------------------------------------------------------
 
 for x in os.listdir(PATH):
     istrue = np.array([x.endswith(file_format)
-                      for file_format in file_formats])
+                       for file_format in file_formats])
     if np.any(istrue):
         # detecting png,jpg and jpeg files which are most common image files (in dataset it has only these filetypes)
         paths.append(PATH+x)
@@ -204,20 +225,24 @@ true_positive = 0
 
 x = 0
 error = 0
-L = 100  # number of images from dataset to test upon
+L = 10  # number of images from dataset to test upon
 index = 0
+t0 = time.time()
+t1 = t0
 
 for path in paths:
+    t2 = t1
     if (x == L):
         '''For L images in this folder'''
         break
     x += 1
     # detection using pre computed Gabor filters
-    detection = detect_copy_move(path, filters)
     stre = images[index].split("_")
     if (len(stre[1]) != 1):
+        # we are skipping bit mask images as they are not images that we have to consider
         x = x-1
     else:
+        detection = detect_copy_move(path, filters)
         real_detected = 1 if stre[1] == "F" else 0
 
         if (real_detected == 0):
@@ -234,7 +259,12 @@ for path in paths:
             else:
                 true_positive += 1
 
+        t1 = time.time()
+
+        print(f"Time elapsed for a image {t1-t0}")
+
     index = index+1
+
 
 accuracy = error/L
 precision = true_positive/(true_positive+false_positive)
@@ -245,3 +275,4 @@ print(f"Accuracy is {accuracy}")
 print(f"Precision is {precision}")
 print(f"Recall is {recall}")
 print(f"F1 score is {f1_score}")
+print(f"Time took is {t1-t0}")
